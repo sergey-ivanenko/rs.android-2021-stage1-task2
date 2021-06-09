f1c4de4b3ac11f63e8c11434e9eead441b6db84b
Sergey Ivanenko <storm06kr@gmail.com>
2021-06-10 00:51:49 +0300

    feat: subtask1 has been done

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,10 +1,26 @@
 package subtask1
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        /*throw NotImplementedError("Not implemented")*/
-        return ""
+        Locale.setDefault(Locale("ru"))
+
+        var dateFormat = SimpleDateFormat("d M yyyy")
+        dateFormat.isLenient = false
+
+        val date: Date? = try {
+            dateFormat.parse("$day $month $year")
+        } catch (e: ParseException) {
+            null
+        }
+
+        dateFormat = SimpleDateFormat("d MMMM, EEEE", Locale.getDefault())
+
+        return if (date != null) dateFormat.format(date) else "Такого дня не существует"
     }
 }
