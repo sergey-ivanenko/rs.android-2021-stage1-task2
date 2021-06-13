843845a59736fb51fbe12f8ba7233a80063a72a5
Sergey Ivanenko <storm06kr@gmail.com>
2021-06-13 10:29:55 +0300

    feat: subtask3 has been done

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,12 +1,48 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.chrono.ChronoLocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-
-        throw NotImplementedError("Not implemented")
+        if (blockA.isEmpty()) {
+            throw IllegalArgumentException("Block can't be empty!")
+        }
+        var result: Any
+        return when(blockB) {
+            Int::class -> {
+                result = 0
+                for (item in blockA) {
+                    if (item is Int) {
+                        result += item
+                    }
+                }
+                result
+            }
+            String::class -> {
+                result = StringBuilder()
+                for (item in blockA) {
+                    if (item is String) {
+                        result.append(item)
+                    }
+                }
+                result.toString()
+            }
+            LocalDate::class -> {
+                result = LocalDate.MIN
+                for (item in blockA) {
+                    if (item is LocalDate && item.isAfter(result as ChronoLocalDate?)) {
+                        result = item
+                    }
+                }
+                result as LocalDate
+                result.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+            }
+            else -> throw IllegalArgumentException("BlockB should be Int, String or Date!")
+        }
     }
 }
